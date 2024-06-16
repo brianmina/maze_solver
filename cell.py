3a66be5a5bdbd9df8f85df9f189e69cd53c3354d
brianmina <brian.mina17@gmail.com>
2024-06-16 10:57:01 -0400

    _draw_cell method

--- cell.py
+++ cell.py
@@ -35,3 +35,29 @@ class Cell:
         if self.has_bottom_wall:
             bottom_wall = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(bottom_wall, "black")
+    
+    # def draw_move(self, to_cell, undo=False):
+    #     start_x, start_y = self.x, self.y  # Get self's center coordinates
+    #     end_x, end_y = to_cell.x, to_cell.y  # Get to_cell's center coordinates
+
+    #     color = "gray" if undo else "red"  # Choose color based on undo flag
+
+    #     # Assume self.draw_line() draws a line between two points (x1, y1) to (x2, y2) with a given color
+    #     self.draw_line(start_x, start_y, end_x, end_y, color)
+
+
+    def draw_move(self, to_cell, undo=False):
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
